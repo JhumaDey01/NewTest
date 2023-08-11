bdd9b599079bbce63a40e482bd46f8fdcb34693c
Administrator <Administrator@172.31.1.209>
2023-08-11 08:55:18 +0000

    more codes

--- src/test/java/test/Googleserach.java
+++ src/test/java/test/Googleserach.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Googleserach {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
@@ -23,6 +25,16 @@ public class Googleserach {
 		driver.navigate().back();
 		String backwordTitle= driver.getTitle();
 		System.out.println("Backword page Title :"+backwordTitle);
+		searchBox.sendKeys("java tutorials");
+		Thread.sleep(5000);
+		List <WebElement> items = driver.findElements(By.xpath("(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']/span"));
+		for(WebElement x : items)
+		{
+			if(x.getText().equalsIgnoreCase("java tutorials"));
+				x.click();
+				break;
+		}
+		driver.quit();
 
 
 
